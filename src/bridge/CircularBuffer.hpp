4858b1254016942f6e06c73c27f05c5629114cde
0forks <114709761+0forks@users.noreply.github.com>
2025-02-21 17:57:06 +0300

    Split implementation from headers

--- src/bridge/CircularBuffer.hpp
+++ src/bridge/CircularBuffer.hpp
@@ -53,16 +53,7 @@ public:
      * @param size Number of bytes to push.
      * @return True if the data was successfully pushed, false if the queue is full.
      */
-    bool Push(const char* data, size_t size) {
-        if (size > BytesFree()) return false;
-        size_t size1 = std::min<size_t>(size, size_ - (head_ % size_));
-        size_t size2 = size - size1;
-        std::memcpy(buffer_.get() + (head_ % size_), data, size1);
-        std::memcpy(buffer_.get(), data + size1, size2);
-        head_ += size;
-        count_ += size;
-        return true;
-    }
+    bool Push(const char* data, size_t size);
 
     /**
      * Pops data from the queue.
@@ -71,16 +62,7 @@ public:
      * @param size Number of bytes to pop.
      * @return True if the data was successfully popped, false if there is not enough data.
      */
-    bool Pop(char* data, size_t size) {
-        if (size > BytesAvailable()) return false;
-        size_t size1 = std::min<size_t>(size, size_ - (tail_ % size_));
-        size_t size2 = size - size1;
-        std::memcpy(data, buffer_.get() + (tail_ % size_ ), size1);
-        std::memcpy(data + size1, buffer_.get(), size2);
-        tail_ += size;
-        count_ -= size;
-        return true;
-    }
+    bool Pop(char* data, size_t size);
 
     /**
      * Copies data from the queue into the given data pointer, without removing it.
@@ -89,15 +71,7 @@ public:
      * @param size Number of bytes to peek.
      * @return Number of bytes actually copied, 0 if there is not enough data.
      */
-    size_t Peek(char* data, size_t size) {
-        size_t available = BytesAvailable();
-        if (size > available) return 0;
-        size_t size1 = std::min<size_t>(size, size_ - (tail_ % size_));
-        size_t size2 = std::min<size_t>(size - size1, available - size1);
-        std::memcpy(data, buffer_.get() + (tail_ % size_), size1);
-        std::memcpy(data + size1, buffer_.get(), size2);
-        return size1 + size2;
-    }
+    size_t Peek(char* data, size_t size);
 
     /**
      * Skips n bytes in the queue.
@@ -105,39 +79,26 @@ public:
      * @param n Number of bytes to skip.
      * @return True if n bytes were successfully skipped, false if n is greater than the number of bytes available in the queue.
      */
-    bool Skip(size_t n) {
-        if (n > BytesAvailable()) return false;
-        tail_ += n;
-        count_ -= n;
-        return true;
-    }
+    bool Skip(size_t n);
 
     /**
      * Clears the queue.
      */
-    void Clear() {
-        head_ = 0;
-        tail_ = 0;
-        count_ = 0;
-    }
+    void Clear();
 
     /**
      * Returns the number of bytes available in the queue.
      * 
      * @return Number of bytes available in the queue.
      */
-    size_t BytesAvailable() const {
-        return count_;
-    }
+    size_t BytesAvailable() const;
 
     /**
      * Returns the number of free bytes in the queue.
      * 
      * @return Number of free bytes in the queue.
      */
-    size_t BytesFree() const {
-        return size_ - BytesAvailable();
-    }
+    size_t BytesFree() const;
 
 private:
     const size_t size_;
